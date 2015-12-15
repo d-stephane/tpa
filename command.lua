01ca6dc44d0e4b68ef9a28088e9e91adb10ced98
BuildTools <stephane@dagda.sd>
2015-12-15 13:17:38 +0100

    Ajout de la fonction find_free_position_near() Log des demandes de TP validées

--- command.lua
+++ command.lua
@@ -2,6 +2,25 @@
 local S = tpa.intllib
 local P = ""
 
+
+-- Copied from Celeron-55's /teleport command. Thanks Celeron!
+local function find_free_position_near(pos)
+    local tries = {
+        {x=1,y=0,z=0},
+        {x=-1,y=0,z=0},
+        {x=0,y=0,z=1},
+        {x=0,y=0,z=-1},
+    }
+    for _,d in pairs(tries) do
+        local p = vector.add(pos, d)
+        if not minetest.registered_nodes[minetest.get_node(p).name].walkable then
+            return p, true
+        end
+    end
+    return pos, false
+end
+
+
 minetest.register_chatcommand("tpa", {
     privs = {
         interact = true
@@ -48,15 +67,19 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
         return false
     end
 
+    local receiver = player:get_player_name()
+
     if fields.button_yes then
         -- Acceptation du TP
-        local player_demandeur = minetest.get_player_by_name(P)
-        player_demandeur:setpos(player:getpos())
+        local sender = minetest.get_player_by_name(P)
+        local pos    = player:getpos()
+        sender:setpos(find_free_position_near(pos))
+        minetest.log("action", "[tpa] Joueur " .. P .. " téléporté vers " .. receiver .. " à la position " .. minetest.serialize(pos))
     else
         -- Refus du TP
         minetest.show_formspec(P, "tpa:no_tp_cancelled",
             "size[7,2]" ..
-            "label[0,0;\"" .. player:get_player_name() .. "\" " .. S("do not want you to teleport to him") .. "]" ..
+            "label[0,0;\"" .. receiver .. "\" " .. S("do not want you to teleport to him") .. "]" ..
             "button_exit[0,1;2,1;exit;" .. S("Close") .. "]")
     end
 
